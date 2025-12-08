122ce4e09d1b452f10e67121fde5f0564cf9526b
Joris Kessels <joris.kessels@eventix.nl>
2025-12-08 12:52:38 +0100

    feat: day7 part2

--- day7/main.go
+++ day7/main.go
@@ -11,6 +11,26 @@ import (
 
 const RUN_EXAMPLE = false
 
+
+var memos = make(map[int]int)
+func bfsLines(x int, field []string, lineIndex int) int {
+	if lineIndex >= len(field) {
+		return 1
+	}
+	if memos[lineIndex * 1000 + x] != 0 {
+		return memos[lineIndex * 1000 + x]
+	}
+	res:=0
+	nextLine := field[lineIndex]
+	if nextLine[x] == '^' {
+		res += bfsLines(x-1, field, lineIndex+1) + bfsLines(x+1, field, lineIndex+1)
+	} else {
+		res += bfsLines(x, field, lineIndex+1)
+	}
+	memos[lineIndex * 1000 + x] = res
+	return res
+}
+
 func main() {
 	fmt.Println("====== Day 7 ======")
 	start := time.Now()
@@ -31,14 +51,18 @@ func main() {
 	part2 := 0
 
 	beamList := make(map[int]bool)
-
+	field := make([]string, 0)
 	for scanner.Scan() {
-		t := scanner.Text()
-		chars := strings.Split(t, "")
+		field = append(field, scanner.Text())
+	}
+
+	for lineIndex, line := range field {
+		chars := strings.Split(line, "")
 		for x, c := range chars {
 			switch c {
 			case "S":
 				beamList[x] = true
+				part2 = bfsLines(x, field, lineIndex + 1)
 			case "^":
 				if beamList[x] {
 					part1++
